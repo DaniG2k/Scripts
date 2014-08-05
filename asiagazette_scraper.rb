c8dd081032b0fc3cda85273045cb67be1f0fee34
Daniele <byakugan.87@gmail.com>
2014-08-05 17:32:00 +0100

    Update asiagazette_scraper.rb

--- asiagazette_scraper.rb
+++ asiagazette_scraper.rb
@@ -6,5 +6,12 @@ urls.each do |url|
   article = Hash.new
   page = Mechanize.new.get(url)
   article['title'] = page.search("//h1").text
+  submitted = page.search("span.submitted").text
   puts line
+end
+
+def parse_submit(s)
+  match = s.scan(/by (.*)? on .*, (.*$)?/).first
+  name = match[0]
+  date = match[1]
 end
