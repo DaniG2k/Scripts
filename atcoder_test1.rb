bb52d2d08164b625745e2e563710f82c5131e3ad
Daniele <byakugan.87@gmail.com>
2015-04-11 14:05:45 +0100

    Create atcoder_test1.rb

---
+++ atcoder_test1.rb
@@ -0,0 +1,17 @@
+nk = gets.chomp.split
+n, k = nk[0].to_i, nk[1].to_i
+balls, moves = [1,2,3,4,5,6,7,8], []
+
+1.upto(n) do |move|
+	moves << gets.chomp.split.collect{|n| n.to_i}
+end
+
+1.upto(k) do |i|
+	moves.each do |move|
+		pos1, pos2 = move[0]-1, move[1]-1
+		tmp1, tmp2 = balls[pos1], balls[pos2]
+		balls[pos1], balls[pos2] = tmp2, tmp1
+	end
+end
+
+puts balls.join ' '
