766d3fd91f74a3d04269e3d8b40e4e9a325e8e8c
Daniele <byakugan.87@gmail.com>
2015-04-23 15:15:18 +0100

    Update mini_crawler.rb

--- mini_crawler.rb
+++ mini_crawler.rb
@@ -3,29 +3,34 @@ require 'mechanize'
 class Crawler
   def initialize(params={})
     @include_patterns = params[:include_patterns]
-    @stop_url_pattern = Regexp.new params[:stop_url_pattern]
-    @visited = []
+    @stop_url_pattern = params[:stop_url_pattern]
+    @visited = Array.new
+    @unwanted_urls = Array.new
   end
-
+  
   def crawl(uri)
     @visited << uri unless @visited.include?(uri)
-    a = Mechanize.new
-    a.get(uri) do |page|
+    
+    agent = Mechanize.new
+    agent.get(uri) do |page|
       puts "Currently scanning '#{page.title}'"
       page.links.each do |link|
-        if stop_pattern_found_in(link.uri.to_s) || stop_pattern_found_in(page.uri.merge(link.uri).to_s)
+        link_str = link.uri.to_s
+        if(stop_pattern_found_in(link_str) || stop_pattern_found_in(page.uri.merge(link.uri).to_s))
           puts "\n! ! !\tStop condition reached\t! ! !"
-          puts "The link was found on #{page.title} with uri #{page.uri}\n"
+          puts "Link #{link_str} was found on '#{page.title}' with uri #{page.uri}\n"
+          # Append to @unwanted_urls list
+          @unwanted_urls << [page.uri.to_s, link_str]
         else
-          l = link.uri.to_s
-          unless @visited.include?(l) || @visited.include?(page.uri.merge(l))
+          unless @visited.include?(link_str) || @visited.include?(page.uri.merge(link.uri).to_s)
             begin
-              crawl(l) if url_crawlable?(l)
+              # The recursive crawl takes place here:
+              crawl(link_str) if url_crawlable?(link_str)
             rescue ArgumentError
-              url = page.uri.merge(l)
-              puts "Relative url found: #{l}"
-              puts "Converting to relative url: #{url}"
-              crawl(url) if url_crawlable?(url.to_s)
+              full_url = page.uri.merge(link_str).to_s
+              puts "Relative url found: #{link_str}"
+              puts "Converting to relative url: #{full_url}"
+              crawl(full_url) if url_crawlable?(full_url)
             rescue Mechanize::UnauthorizedError
               puts "Unauthorized access: #{link.uri}"
             rescue Mechanize::ResponseCodeError
@@ -33,7 +38,7 @@ class Crawler
             rescue URI::InvalidURIError
               puts "Invalid URI: #{link.uri}"
               puts "Trying again with URI escaping..."
-              crawl URI.escape(l)
+              crawl URI.escape(link_str)
             rescue => e
               puts e
             end
@@ -41,6 +46,14 @@ class Crawler
         end
       end
     end
+  end
+
+  def visited
+    @visited
+  end
+
+  def unwanted_urls
+    @unwanted_urls
   end
 
   private
@@ -54,5 +67,10 @@ class Crawler
 end
 
 ucl = Crawler.new(:include_patterns => ['www.ucl.ac.uk/'],
-                  :stop_url_pattern => '/ucl.ac.uk')
+                  :stop_url_pattern => /\/ucl.ac.uk\/?$/)
 ucl.crawl 'http://www.ucl.ac.uk/lifelearning/'
+
+puts "\n Visited urls:"
+p ucl.visited
+puts "\n Unwanted urls:"
+p ucl.unwanted_urls
